6d68aa5c120fbed25a50b36486f0c2aefd3fd61c
Mark Hu <mark.vc.hu@gmail.com>
2015-01-03 10:08:24 +0800

    Implemented signing in.

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,14 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
-
-  get 'users/create'
-
-  get 'users/show'
-
   root 'projects#index'
   resources :projects do
     resources :tickets
   end
 
   resources :users
+
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
