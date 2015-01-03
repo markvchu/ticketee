e6ab92352000e06cc3dc659799ae846f5ef389b5
Mark Hu <mark.vc.hu@gmail.com>
2015-01-03 19:55:14 +0800

    Add ability to edit and update users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,7 @@
 class Admin::UsersController < Admin::BaseController
+
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
   def index
     @users = User.order(:email)
   end
@@ -20,8 +23,34 @@ class Admin::UsersController < Admin::BaseController
     end
   end
 
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+    if params[:user][:password].blank?
+      params[:user].delete(:password)
+      params[:user].delete(:password_confirmation)
+    end
+
+    if @user.update(user_params)
+      redirect_to admin_users_path, notice: 'User has been updated.'
+    else
+      flash[:alert] = 'User has not been updated.'
+      render 'edit'
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :admin)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :name, presence: true
+  validates :email, presence: true
+
 
   def to_s
     "#{email} (#{admin? ? 'Admin' : 'User'})"
