adb0ddcc1b475fcde04e7612e5b9d8b3f3799b23
Mark Hu <mark.vc.hu@gmail.com>
2015-01-06 04:22:53 +0800

    Users can now upload 3 files at a time

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150105140557) do
+ActiveRecord::Schema.define(version: 20150105193327) do
+
+  create_table "assets", force: true do |t|
+    t.string   "asset"
+    t.integer  "ticket_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "permissions", force: true do |t|
     t.integer  "user_id"
